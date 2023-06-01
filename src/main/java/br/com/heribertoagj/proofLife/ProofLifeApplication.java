1ceb59d9a13285cab28ee2c4c8185a87b265524b
heribertoagj <heribertoagj@gmail.com>
2023-06-01 09:25:01 -0300

    included initial files

---
+++ src/main/java/br/com/heribertoagj/proofLife/ProofLifeApplication.java
@@ -0,0 +1,13 @@
+package br.com.heribertoagj.proofLife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProofLifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProofLifeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/heribertoagj/proofLife/ProofLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.heribertoagj.proofLife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProofLifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
